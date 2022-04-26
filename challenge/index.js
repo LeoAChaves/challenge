10432d05dc9eb9b03a40897bcf68fb89671cb346
LeoAChaves <leochaves2121@gmail.com>
2022-04-26 12:16:18 -0300

    Creating an obj for each "headers" and "property"

--- challenge/index.js
+++ challenge/index.js
@@ -1,15 +1,15 @@
 import fs from "fs";
 
-// function createJsonFile(fileName, fileContent) {
-//   fs.writeFile(
-//     `./challenge/${fileName}.json`,
-//     JSON.stringify(fileContent),
-//     (error) => {
-//       if (error) throw error;
-//       console.log("\nDone writing\n");
-//     }
-//   );
-// }
+function createJsonFile(fileName, fileContent) {
+  fs.writeFile(
+    `./challenge/${fileName}.json`,
+    JSON.stringify(fileContent),
+    (error) => {
+      if (error) throw error;
+      console.log("\nDone writing\n");
+    }
+  );
+}
 
 const csv = fs.readFileSync("./challenge/input.csv");
 const csvArray = csv.toString().split("\r").toString().split("\n");
@@ -23,12 +23,15 @@ const headers = csvArray[0]
 headers.pop();
 
 for (let i = 1; i < csvArray.length; i++) {
+  let obj = {};
   let property = csvArray[i]
     .split(", ")
     .toString()
     .replaceAll('"', "")
     .split(",");
   property.pop();
-
-  console.log(`Property ${i}: ${property}`);
+  for (let j = 0; j < headers.length; j++) {
+    obj[headers[j]] = property[j];
+  }
+  console.log(`Object ${i}: ${JSON.stringify(obj)}\n`);
 }
